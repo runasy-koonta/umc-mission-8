2cd60c111204f9821f0d345dc9afa396eea41f76
kaaang <kaaang@local>
2023-11-23 16:52:23 +0900

    feat: Add error API

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import { tempRouter } from './routes/temp.route.js';
 const app = express();
 const port = 3000;
 
-// router setting
 app.use('/temp', tempRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send(err.stack);
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- src/services/temp.service.js
+++ src/services/temp.service.js
@@ -1,5 +1,13 @@
-import { tempResponseDTO } from "../dtos/temp.response.dto.js";
+import { tempResponseDTO, flagResponseDTO } from "../dtos/temp.response.dto.js";
 
 export const getTempData = () => {
     return tempResponseDTO("This is TEST! >.0");
+}
+
+export function CheckFlag(flag){
+    if(flag == 1)
+        throw new Error("Flag is 1!!");   // 에러 발생시키기!
+    else{
+        return flagResponseDTO(flag);
+    }
 }
